1233be1daa6b57104ea56908162c70aa9e58796c
Diego Estanislau <diegoperrone2018@gmail.com>
2021-01-04 14:00:58 -0300

    Start Project

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { Ionicons } from "@expo/vector-icons";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open App.js to start working on your app!</Text>
       <Ionicons name="md-checkmark-circle" size={32} color="green" />
       <StatusBar style="auto" />
     </View>
