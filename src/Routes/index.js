7bfa58c3162982bd12a79f4693f36208ac8efb10
Diego Estanislau <diegoperrone2018@gmail.com>
2021-01-04 14:24:49 -0300

    Created Navigation

---
+++ src/Routes/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Started from "../pages/Started";
+
+const Stack = createStackNavigator();
+
+const Routes = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Started" component={Started} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
